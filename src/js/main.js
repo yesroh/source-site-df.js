dd1e4b055f4368607a2408bc327c0fa776a8c419
Wendell Adriel <wendell@codeshare.com.br>
2016-03-11 17:49:45 -0300

    Added sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+document.getElementById('btn-goabout').addEventListener('click', function() {
+  var top = document.getElementById('sec-about').offsetTop;
+  window.scroll(0, top);
+});
+
+document.getElementById('btn-goevents').addEventListener('click', function() {
+  var top = document.getElementById('sec-events').offsetTop;
+  window.scroll(0, top);
+});
+
+document.getElementById('btn-goadmins').addEventListener('click', function() {
+  var top = document.getElementById('sec-admins').offsetTop;
+  window.scroll(0, top);
+});
+
+document.getElementById('btn-gotop').addEventListener('click', function() {
+  window.scroll(0, 0);
+});
